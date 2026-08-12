ba4de4a1ff29e39edcf21ce5d4aa19949779ff86
Tantrina Chaudhuri <tantrina.chaudhuri@winwire.com>
2026-08-12 15:44:32 +0530

    update pending pipeline and rejected

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ let approved = 234;
 let rejectedcount=0;
 let skills = [];
 let rejectedskills=[];
+let approvedskills=[];
 let create = document.querySelector(".create-skill button");
 let skillModal = document.querySelector(".skill-modal");
 let skillInput = document.querySelector(".skill-input");
 let approvedCard = document.querySelector(".approved-skills");
 let added = document.querySelector("#addedskills .skills-container");
 let approvedList = document.querySelector("#approvedskills .skills-container");
+let rejectedList=document.getElementById("rejectedskills");
 let btn = document.getElementById("add-skill");
 let initial=350;
 let app = document.getElementById("app-skills");
 let iniapp=234;
-let pen=document.getElementById("pending");
+let pen=document.getElementById("pipeline-pending");
+let pen_card=document.getElementById("pending");
 let feedbackskillModal = document.querySelector(".feedback-modal");
 let feedBtn = document.getElementById("feedback-btn");
 let feedmodal=document.getElementById("add-feedback");
@@ -62,26 +65,42 @@ function showSkills() {
 }
 
 app.addEventListener("click", function () {
-    approvedList.innerHTML = "";
-    skills.forEach(function (name) {
-        let div = document.createElement("div");
-        div.innerText = name;
-        approvedList.appendChild(div);
-    });
-    iniapp++;
-    let count=document.getElementById("approved-skills");
-    approval_pipeline.innerHTML = `Approved <b>${iniapp}</b>`;
-    count.innerText=iniapp;
-    let currentcount=initial-iniapp;
-    pen.innerText=currentcount;
-    alert("Skills approved")
+    if(skillInput.value.trim()==""){
+        alert("Enter a skill to approve or reject")
+    }else{
+        approvedList.innerHTML = "";
+        approvedskills.push(skillInput.value.trim());
+        approvedskills.forEach(function (name) {
+            let div = document.createElement("div");
+            div.innerText = name;
+            approvedList.appendChild(div);
+        });
+        iniapp++;
+        let count=document.getElementById("approved-skills");
+        approval_pipeline.innerHTML = `Approved <b>${iniapp}</b>`;
+        count.innerText=iniapp;
+        let currentcount=initial-iniapp;
+        pen.innerHTML = `Pending <b>${currentcount}</b>`;
+        pen_card.innerText=currentcount;
+        alert("Skills approved")
+    }
+   
 });
 
 rej_btn.addEventListener("click",function(){
-    rejectedskills.push(skillInput.value.trim());
-    console.log(rejectedskills);
-    rejectedcount++;
-    rejected.innerText=rejectedcount;
-    rej_pipeline.innerText=rejectedcount;
-
+    if(skillInput.value.trim()==""){
+        alert("Enter a skill to approve or reject")
+    }else{
+        rejectedskills.push(skillInput.value.trim());
+        rejectedskills.forEach(list=>{
+            let div=document.createElement("div");
+            div.innerText=list;
+            rejectedList.appendChild(div);
+        })
+        rejectedcount++;
+        rejected.innerText=rejectedcount;
+        rej_pipeline.innerText=rejectedcount;
+        rejectedskills.push(skillInput.value.trim());
+        console.log(rejectedskills);
+    }
 })
